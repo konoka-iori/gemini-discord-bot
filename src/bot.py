9cd8c20075f6a9fe6772f006b37c0e21268ba64f
Konoka-Iori <Konoka-Iori@parity-blog.info>
2024-01-31 11:43:27 +0900

    `/reply` を仮実装したが、動作しない。 #2 多分Discordの仕様上によるもの？さらに検証が必要。詳細は #2

--- src/bot.py
+++ src/bot.py
@@ -30,6 +30,13 @@ async def chat(ctx:discord.Interaction, message:str) -> None:
         await ctx.response.defer(thinking=True)
         await ctx.followup.send(chat_data.get_response(message))
 
+# BUG:ctx.message.reference.resolved.contentがNoneになる
+# @tree.command(name="reply", description=command_data.get_command_description("reply"), guild=DISCORD_SERVER_ID)
+# async def reply(ctx:discord.Interaction) -> None:
+#     async with ctx.channel.typing():
+#         await ctx.response.defer(thinking=True)
+#         await ctx.followup.send(chat_data.get_response(ctx.message.reference.resolved.content))
+
 
 @client.event
 async def on_ready() -> None:
