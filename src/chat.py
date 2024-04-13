57156c82e37eb856a46666748a0055e44b2cd12a
Konoka Iori <konoka-iori@parity-blog.info>
2024-04-13 18:16:11 +0900

    Refactor chat.py to use gemini's generate_content method

--- src/chat.py
+++ src/chat.py
@@ -9,13 +9,13 @@ class Chat:
             gemini.configure(api_key=self.token)
             config = {"max_output_tokens": 1000}
             model = gemini.GenerativeModel(model_name="gemini-1.5-pro-latest", generation_config=config)
-            chat = model.start_chat()
-            return str(chat.send_message(message).text)
+            return str(model.generate_content(message).text)
         except Exception as e:
-            return f"エラーが発生しました。以下の内容をコピペして管理者までお知らせください。\n```{e}```" # TODO: JSONに移すか検討。今はとりあえずこれで。
+            return f"エラーが発生しました。以下の内容をコピペして管理者までお知らせください。\n```{e}```"
 
 
 if __name__ == "__main__":
     from os import getenv
     chat = Chat(getenv("GEMINI_API_KEY"))
-    print(chat.get_response(input(">>> ")))
+    print(chat.get_response("自己紹介してください。"))
+    #print(chat.get_response(input(">>> ")))
