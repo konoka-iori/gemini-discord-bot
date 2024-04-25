60b671807f218ec13ad37a9e0aa751fc2e5dfe0a
Konoka Iori <konoka-iori@parity-blog.info>
2024-04-25 14:01:11 +0900

    `ping` 関数を追加

--- src/chat.py
+++ src/chat.py
@@ -19,10 +19,18 @@ class Chat:
         except Exception as e:
             return f"エラーが発生しました。以下の内容をコピペして管理者までお知らせください。\n```{e}```", float(0)
 
+    def ping(self) -> str:
+        response = self.get_response("ping")
+        if response[1] == 0:
+            return str(response[0])
+        else:
+            return f"{round(response[1], 2)} ms"
+
 
 if __name__ == "__main__":
     from os import getenv
     chat = Chat(token=getenv("GEMINI_API_KEY"), model="gemini-1.5-pro-latest") #type: ignore
     test_response = chat.get_response("自己紹介してください。")
     print(test_response[0] + f"\n処理時間: {test_response[1]}")
+    print(chat.ping())
     #print(chat.get_response(input(">>> ")))
